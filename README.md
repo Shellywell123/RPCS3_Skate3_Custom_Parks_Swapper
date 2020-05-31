06e82883c4908d806f9ecca3c502dc27cd802308
Shellywell123 <benjamin@shellswell.com>
2020-05-31 12:44:42 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+#  RPCS3_Skate3_Custom_Parks_Swapper
+(!THIS IS STILL IN DEVELOPMENT, IF YOU ARE GOING TO USE THIS PROGRAM, BACKUP ALL YOUR CUSTOM SKATEPARK FILES FIRST TO A SEPERATE FOLDER ON YOU PC!)\
+If like me you have recently moved from gaming on your PS3 console to to a PC and are now using RPCS3 to continue playing the best game in the world (Skate3). You may also have multiple custom skatepark files, ones which you have made on your old PS3 and ones which you have since made on your PC. This program is to be used for easily swapping out the custom skate park files in the RPCS3 emulator to be played in the game. As my knowlege thus far is that you can only have one skateparks file at a time, and not sure there is a way to combine them.
+(Note this program is for swapping between skatepark save files you already own, using the basic commands in a linux terminal i.e 'cp'.)
+
+### How to setup:
+In the python script you need to change the the paths to be relative to the folders on your PC (I will leave mine in script as an example):
+
+```python
+#paths for python
+parks_repo_path = "path to where your multiple park files are stored"
+os_parks_repo_path = "same as above with appropriate '\'s in path "
+
+#paths for terminal
+rpcs3_park_folder_path = 'path to RPCS3/dev_hdd0/home/00000001/savedata/BLES00760-SPARK_SKATER/'
+    #may be BLUS is you have US ver of skate3
+os_rpcs3_park_folder_path = "same as above with appropriate '\'s in path "
+```
+(Note this program expects the custom park files to extracted in to folders, alongside a txt containing the name of the current park in the RPCS3 folder)\
+
+### How to run:
+Once you have completed the set up instuctions above, execute the python script using python3:
+```bash
+foo@bar:~$ python3 swap_custom_parks_out.py
+--------------------------------------------------------------
+   __|  |  /    \ __ __| __| __ /    _ \  \    _ \  |  /   __|
+ \__ \  . <    _ \   |   _|   _ \    __/ _ \     /  . <  \__ \
+ ____/ _|\_\ _/  _\ _|  ___| ___/   _| _/  _\ _|_\ _|\_\ ____/
+ --------------------------------------------------------------
+
+
+Current park save in game:
+ - Bens_Razer_Blade_Parks
+Updated Bens_Razer_Blade_Parks in Repo
+
+Save parks in repository:
+ - Bens_PS3_Slim_Parks
+ - Bens_Razer_Blade_Parks
+ - Dizeeus_Skateparks
+ - EU_Skateparks
+ - EU_Top_Rated
+ - GHFears_Server_Dump
+ - Untreated_Park_files
+```
+The program will then tell you which park is currently in your RPCS3 folder and save any changes you have made to it while playing before swapping it. The program will also list all the custom park folders you can swap from and ask you to chose one via a user input. In the example script below 'Bens_PS3_Slim_Parks' was chosen.
+```bash
+Input Which Park_Saves you want to play:
+>> Bens_PS3_Slim_Parks
+
+Emptied RPCS3 folder
+Bens_PS3_Slim_Parks files placed into RPCS3 folder
+--------------------------------------------------------------
+```
+The program then empties your RPCS3 custom park folder and copies in the files from the chosen folder you inputted.\
+Your skate 3 game should now have different custom parks when you play it in RPCS3 :)
---
+++ swap_custom_parks_out.py
@@ -0,0 +1,99 @@
+import os
+
+#paths for python
+parks_repo_path = "/mnt/c/Users/benja/Documents/Entertainment/Gaming/ROM's/Playstation 3/DLC's and Extras/Skate 3/Skate 3 Custom Parks/"
+os_parks_repo_path = "/mnt/c/Users/benja/Documents/Entertainment/Gaming/ROM\\'s/Playstation\\ 3/DLC\\'s\\ and\\ Extras/Skate\\ 3/Skate\\ 3\\ Custom\\ Parks/"
+
+#paths for terminal
+rpcs3_park_folder_path = '/mnt/c/Users/benja/Documents/Entertainment/Gaming/Launchers/PS3/dev_hdd0/home/00000001/savedata/BLES00760-SPARK_SKATER/'
+os_rpcs3_park_folder_path = '/mnt/c/Users/benja/Documents/Entertainment/Gaming/Launchers/PS3/dev_hdd0/home/00000001/savedata/BLES00760-SPARK_SKATER/'
+
+#########################################################################
+
+def ascii():
+    """ascii art for begining og program"""
+    print("""--------------------------------------------------------------
+   __|  |  /    \ __ __| __| __ /    _ \  \    _ \  |  /   __| 
+ \__ \  . <    _ \   |   _|   _ \    __/ _ \     /  . <  \__ \ 
+ ____/ _|\_\ _/  _\ _|  ___| ___/   _| _/  _\ _|_\ _|\_\ ____/ 
+ --------------------------------------------------------------
+""")
+
+
+def set_current_save_parks(newname):
+    """sets txt log to be the name of the current skate parks file in the RPCS3 folder"""
+    file = parks_repo_path+'CPIRPCS(DoNotDelete).txt'
+
+    with open(file, 'w') as f:
+        f.write(newname)
+
+def get_current_save_parks():
+    """ retrieve the name of the current skateparks file in the RPCS3 file from the log txt"""
+    file = parks_repo_path+'CPIRPCS(DoNotDelete).txt'
+
+    with open(file, 'r') as f:
+       cont = f.read()
+    print('\nCurrent park save in game:')
+    print(' - '+cont)
+    return cont
+
+def update_park_save_changes():
+    """ performs a backup of any changes to a skate park file in the  RPCS3 folder before it is swapped out """
+    current_park_save = get_current_save_parks()
+    try:
+        osstr = 'cp {}* {}'.format(os_rpcs3_park_folder_path,os_parks_repo_path+current_park_save+'/')
+        os.system(osstr)
+        print('Updated {} in Repo'.format(current_park_save))
+    except:
+        print('Updated of {} in Repo FAILED'.format(current_park_save))
+        exit(0)
+
+def empty_rpcs3():
+    """empties the RPCS3 folder before a differrent save park file is swapped in"""
+    os.system('rm {}*'.format(os_rpcs3_park_folder_path))
+    print('Emptied RPCS3 folder')
+
+def print_files_in_rpcs3_folder():
+    """shows the user the contents of the RPCS3 folder"""
+    for d in os.listdir(rpcs3_park_folder_path) :
+        print(' - '+str(d))
+
+def list_park_saves_in_repo():
+    """shows the user the possible options of save park files he can swap between"""
+    print('\nSave parks in repository:')
+
+    repo_list = []
+    for d in os.listdir(parks_repo_path) :
+        if '.' not in d:
+            print(' - '+str(d))
+            repo_list.append(d)
+
+    return repo_list
+
+def swap_park_saves():
+    """swaps out custom skate park saves in RPCS3 folder via a user input"""
+    update_park_save_changes()
+    
+    choice_list = list_park_saves_in_repo()
+
+    choice = input('\nInput Which Park_Saves you want to play:\n')
+    print('')
+
+    if choice == 'exit':
+        exit(0)
+
+    if choice in choice_list:
+        empty_rpcs3()
+        ostr = "cp {}* {}".format(os_parks_repo_path+choice+'/',os_rpcs3_park_folder_path)
+        os.system(ostr)
+        print('{} files placed into RPCS3 folder'.format(choice))
+        set_current_save_parks(choice)
+        print('--------------------------------------------------------------')
+    else:
+        print('not a valid input')
+
+#########################################################################
+
+# run this
+ascii()
+swap_park_saves()
